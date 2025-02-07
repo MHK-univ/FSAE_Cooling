670a931af1f26cab4e6200ad4afd551d86980697
minho <minho@Minho_HUB>
2025-02-07 21:45:42 +0900

    Chnage Variables

--- Start.m
+++ Start.m
@@ -1,18 +1,22 @@
-%% 
+%% 'Start.m' must be executed before starting the simulation. 
+% Here, we define the desired variables, which serve as the basis for the simulation.
+% I have included an example at the beginning. You can modify it as needed in the future
 
 clear;
 
-t_sim = 1697;
+%% Setting the simulation time
+% Enter the final time recorded in AIM or ECU-Master.
 
-%% 
+t_sim = 1697; % Unit [s]
 
-R_h = 352 ; % [mm]
-R_l = 302 ; % [mm]
-R_t = 32 ; % [mm]
-Fin_h = 4.85 ; % [mm]
-FPI = 16 ; % [mm]
+%% Setting radiator geometry variables
+% Enter the geometry variables of the radiator you want to use
 
-Matrix_parameter = [R_h; R_l; R_t; Fin_h; FPI;];
+R_h = 352 ; % Radiator Core Height [mm]
+R_l = 302 ; % Radiator Core Length [mm]
+R_t = 32 ; % Radiator Core Thickness [mm]
+Fin_h = 4.85 ; % Fin height [mm]
+FPI = 16 ; % Fin per inch [/inch]
 
-save('Radi_parameter.mat', 'Matrix_parameter', '');
+save('Radi_parameter.mat', 'R_h', 'R_l', "R_t", "Fin_h", "FPI", '-mat');
 
